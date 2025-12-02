cca4918c8ee270a4e044647d9cebae859e4cec62
nandosolis15-max <nandosolis15@icloud.com>
2025-12-02 19:35:36 +0000

    Week 16 #5

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -12,3 +12,16 @@
 
 # Starter Code:
 
+Temp = int(input("What is the temperature today?: "))
+if Temp <= -10: 
+    print("Extreme temperature warning!")
+elif Temp >= -9 and Temp <= 39:
+    print("COLD")
+elif Temp >= 40 and Temp <= 55:
+    print("CHILLY ")
+elif Temp >= 56 and Temp <= 65:
+    print("WARM")
+elif Temp >= 66 and Temp <= 109:
+    print("HOT")
+else:
+    print("Extreme temperature warning!")
