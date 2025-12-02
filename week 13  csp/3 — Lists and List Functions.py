6bab0f4cfbb6005fef398bde509aee1443448166
nandosolis15-max <nandosolis15@icloud.com>
2025-12-02 19:15:17 +0000

    Nest Lists

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -36,42 +36,42 @@ print(type(lists_of_fruits))
 
 print(lists_of_fruits[0]) 
 print(lists_of_fruits[1])
-print(lists_of_fruits[-1])
+print(lists_of_fruits[2])
 print(lists_of_fruits[1:3])
-lists_of_fruits.reverse()
-print(lists_of_fruits)
-print(lists_of_fruits[::-1])
-lists_of_fruits.append("elderberry")
-print(lists_of_fruits)
+# lists_of_fruits.reverse()
+# print(lists_of_fruits)
+# print(lists_of_fruits[::-1])
+# lists_of_fruits.append("elderberry")
+# print(lists_of_fruits)
 
-lists_of_fruits.extend(["mango", "pear", "strawberry"])
-print(lists_of_fruits)
+# lists_of_fruits.extend(["mango", "pear", "strawberry"])
+# print(lists_of_fruits)
 
-lists_of_fruits.remove("banana")
-print(lists_of_fruits)
-lists_of_fruits.insert(3, "banana")
-lists_of_fruits.sort()
-print(lists_of_fruits)
-lists_of_items = list(range(1, 1001))
-print(lists_of_items)
-print(len(lists_of_items))
-lists_of_items.extend(range(1001, 2001))
-print(len(lists_of_items))
+# lists_of_fruits.remove("banana")
+# print(lists_of_fruits)
+# lists_of_fruits.insert(3, "banana")
+# lists_of_fruits.sort()
+# print(lists_of_fruits)
+# lists_of_items = list(range(1, 1001))
+# print(lists_of_items)
+# # print(len(lists_of_items))
+# # lists_of_items.extend(range(1001, 2001))
+# # print(len(lists_of_items))
  
-#why use a list
-#instead of creating seperate variables 
-# for each item, we can store them in a list
-# this wakes our job easier
-# this is
+# #why use a list
+# #instead of creating seperate variables 
+# # for each item, we can store them in a list
+# # this wakes our job easier
+# # this is
 
-set1= {1,2, 3 , 4, 4, 5}
+# set1= {1,2, 3 , 4, 4, 5}
 
-# Print the second and last item.
+# # Print the second and last item.
 
-# Add a new item using .append().
+# # Add a new item using .append().
 
-# Remove the first item using .pop(0).
+# # Remove the first item using .pop(0).
 
-# Reverse your list using .reverse().
+# # Reverse your list using .reverse().
 
-# Create a list of 3 lists (matrix), and access the middle element.
+# # Create a list of 3 lists (matrix), and access the middle element.
--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -1,6 +1,43 @@
 # Objective:
 # Students will manipulate nested lists and understand basic list comprehensions.
 
+list1 = [1, 2, 3]
+print (list1[-1])
+list2 = [4, 5, 6]
+print(list2[0])
+nested_list = [list1, list2]
+print(nested_list[0])
+print(nested_list[1])
+print(nested_list [1][2])
+
+
+
+fruits = ["apples", "oranges", "banana", "coconut"]
+vegetables = ["celery", "carrot"," potatoes"]
+meats = ["chicken", "fish", "turkey"]
+groceries = [fruits, vegetables, meats]
+
+print(groceries[1][1])
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ")
+    print()
+
+# for i in range (1,101):
+#     for j in range(1,101):
+#         if i > 0 and j > 0:
+#             for k in range(1,101):
+#                 print("the number is ", i)
+
+num_pad = ((1,2,3),
+           (4,5,6),
+           (7,8,9),
+           ("*",0,"#"))
+for row in num_pad:
+    for num in row:
+        print(num, end=" ") 
+    print()  
+
 # Key Notes:
 
 # A list can contain other lists.
@@ -16,30 +53,39 @@
 
 # List comprehensions provide a concise way to create lists.
 
-# Examples:
+# # Examples:
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
 ]
 
-print(matrix[1][2])    # 6
+print(matrix[0])    # 6
+print(matrix[2][1])
 
-# List comprehension
-first_col = [row[0] for row in matrix]
-print(first_col)       # [1, 4, 7]
+sum_list = [row[-1] for row in matrix]
+print(sum_list)
+
+
+# # List comprehension
+# first_col = [row[0] for row in matrix]
+# print(first_col)       # [1, 4, 7]
 
 
 
-# Practice Problems:
+# # Practice Problems:
 
-# Build a matrix variable containing 3 lists of 3 numbers each.
+# # Build a matrix variable containing 3 lists of 3 numbers each.
 
-# Print the first list.
+# # Print the first list.
 
-# Print the second item from the third list.
+# # Print the second item from the third list.
 
-# Use a list comprehension to extract the last item from each sub-list.
+# # Use a list comprehension to extract the last item from each sub-list.
 
-# Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
+# # Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
+
+
+squares = [x**2 for x in range (1, 11)]
+print(squares)
